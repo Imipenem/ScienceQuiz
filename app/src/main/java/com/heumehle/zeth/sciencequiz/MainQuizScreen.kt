83386c8f14e432d4e75a100cc2f8c193b28bb2b6
Imipenem <philipp.ehmele@yahoo.de>
2018-04-25 20:53:24 +0200

    TotalPoints now working

--- app/src/main/java/com/heumehle/zeth/sciencequiz/MainQuizScreen.kt
+++ app/src/main/java/com/heumehle/zeth/sciencequiz/MainQuizScreen.kt
@@ -1,20 +1,27 @@
 package com.heumehle.zeth.sciencequiz
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main_quiz_screen.*
+
 
 class MainQuizScreen : AppCompatActivity() {
 
-    TextView TextField;
+    private var totalPoints: Int = 0
 
-    Button A_Button;
-    Button B_Button;
-    Button C_Button;
-    Button D_Button;
+    internal var TextField: TextView
+    internal var PointsField: TextView
+    internal var A_Button: Button
+    internal var B_Button: Button
+    internal var C_Button: Button
+    internal var D_Button: Button
 
-    Question dummyQuestion = new Question("Korrekt", "Frage", "Falsch1", "Falsch2" , "Falsch3");
+    internal var dummyQuestion = Question("Korrekt", "Frage", "Falsch1", "Falsch2", "Falsch3")
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -43,85 +50,87 @@ class MainQuizScreen : AppCompatActivity() {
         createQuestionScreen();
     }
 
-    public void createQuestionScreen()
-    {
+    fun createQuestionScreen() {
 
-        TextField = findViewById(R.id.textView2);
-        TextField.setText(dummyQuestion.getM_question());
-        A_Button = findViewById(R.id.answer_A);
-        B_Button = findViewById(R.id.answer_B);
-        C_Button = findViewById(R.id.Answer_C);
-        D_Button = findViewById(R.id.Answer_D);
+        TextField = findViewById<TextView>(R.id.textView2)
+        TextField.setText(dummyQuestion.getM_question())
+        PointsField = findViewById<TextView>(R.id.allPoints)
+        PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
 
 
-        int rand =(int)((Math.random() * 4) + 1);
+        A_Button = findViewById<Button>(R.id.answer_A)
+        B_Button = findViewById<Button>(R.id.answer_B)
+        C_Button = findViewById<Button>(R.id.Answer_C)
+        D_Button = findViewById<Button>(R.id.Answer_D)
 
-        switch(rand) {
 
-            case 1:
-            A_Button.setText(dummyQuestion.getM_correctAnswer());
-            B_Button.setText(dummyQuestion.getM_false1());
-            C_Button.setText(dummyQuestion.getM_false2());
-            D_Button.setText(dummyQuestion.getM_false3());
-            break;
+        val rand = (Math.random() * 4 + 1).toInt()
 
-            case 2:
-            B_Button.setText(dummyQuestion.getM_correctAnswer());
-            A_Button.setText(dummyQuestion.getM_false3());
-            C_Button.setText(dummyQuestion.getM_false2());
-            D_Button.setText(dummyQuestion.getM_false1());
-            break;
+        when (rand) {
 
-            case 3:
-            C_Button.setText(dummyQuestion.getM_correctAnswer());
-            A_Button.setText(dummyQuestion.getM_false1());
-            B_Button.setText(dummyQuestion.getM_false2());
-            D_Button.setText(dummyQuestion.getM_false3());
-            break;
+            1 -> {
+                A_Button.setText(dummyQuestion.getM_correctAnswer())
+                B_Button.setText(dummyQuestion.getM_false1())
+                C_Button.setText(dummyQuestion.getM_false2())
+                D_Button.setText(dummyQuestion.getM_false3())
+            }
 
-            case 4:
-            D_Button.setText(dummyQuestion.getM_correctAnswer());
-            A_Button.setText(dummyQuestion.getM_false2());
-            B_Button.setText(dummyQuestion.getM_false3());
-            C_Button.setText(dummyQuestion.getM_false1());
-            break;
+            2 -> {
+                B_Button.setText(dummyQuestion.getM_correctAnswer())
+                A_Button.setText(dummyQuestion.getM_false3())
+                C_Button.setText(dummyQuestion.getM_false2())
+                D_Button.setText(dummyQuestion.getM_false1())
+            }
 
+            3 -> {
+                C_Button.setText(dummyQuestion.getM_correctAnswer())
+                A_Button.setText(dummyQuestion.getM_false1())
+                B_Button.setText(dummyQuestion.getM_false2())
+                D_Button.setText(dummyQuestion.getM_false3())
+            }
+
+            4 -> {
+                D_Button.setText(dummyQuestion.getM_correctAnswer())
+                A_Button.setText(dummyQuestion.getM_false2())
+                B_Button.setText(dummyQuestion.getM_false3())
+                C_Button.setText(dummyQuestion.getM_false1())
+            }
         }
 
-        A_Button.setOnClickListener(new View . OnClickListener () {
-            public void onClick(View v) {
-                if (A_Button.getText().equals(dummyQuestion.getM_correctAnswer())) {
-                    A_Button.setBackgroundColor(Color.GREEN);
-                } else A_Button.setBackgroundColor(Color.RED);
-            }
-        });
+        A_Button.setOnClickListener(View.OnClickListener {
+            if (A_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+                A_Button.setBackgroundColor(Color.GREEN)
+                totalPoints++
+                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
+            } else
+                A_Button.setBackgroundColor(Color.RED)
+        })
 
-        B_Button.setOnClickListener(new View . OnClickListener () {
-            public void onClick(View v) {
-                if (B_Button.getText().equals(dummyQuestion.getM_correctAnswer())) {
-                    B_Button.setBackgroundColor(Color.GREEN);
-                } else B_Button.setBackgroundColor(Color.RED);
-            }
-        });
+        B_Button.setOnClickListener(View.OnClickListener {
+            if (B_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+                B_Button.setBackgroundColor(Color.GREEN)
+                totalPoints++
+                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
+            } else
+                B_Button.setBackgroundColor(Color.RED)
+        })
 
-        C_Button.setOnClickListener(new View . OnClickListener () {
-            public void onClick(View v) {
-                if (C_Button.getText().equals(dummyQuestion.getM_correctAnswer())) {
-                    C_Button.setBackgroundColor(Color.GREEN);
-                } else C_Button.setBackgroundColor(Color.RED);
-            }
-        });
+        C_Button.setOnClickListener(View.OnClickListener {
+            if (C_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+                C_Button.setBackgroundColor(Color.GREEN)
+                totalPoints++
+                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
+            } else
+                C_Button.setBackgroundColor(Color.RED)
+        })
 
-        D_Button.setOnClickListener(new View . OnClickListener () {
-            public void onClick(View v) {
-                if (D_Button.getText().equals(dummyQuestion.getM_correctAnswer())) {
-                    D_Button.setBackgroundColor(Color.GREEN);
-                } else D_Button.setBackgroundColor(Color.RED);
-            }
-        });
+        D_Button.setOnClickListener(View.OnClickListener {
+            if (D_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+                D_Button.setBackgroundColor(Color.GREEN)
+                totalPoints++
+                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
+            } else
+                D_Button.setBackgroundColor(Color.RED)
+        })
     }
-}
-
-
-
 }
