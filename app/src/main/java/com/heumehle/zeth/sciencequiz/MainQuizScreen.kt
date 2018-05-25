8966f8f3597ee564953d3e2aadeb7c98e484df45
Imipenem <philipp.ehmele@yahoo.de>
2018-05-25 21:51:38 +0200

    -Modified constraint Layout to work for different size screens
    -Modified OnKlickListener: If Users answer is not correct, a new question appears
    
    -Added a new Activity, when User´s answer is correct
    
    Problems: need for refactoring, some Bugs (sometimes at the start the App crashes with the need of restart the App),better way for creating new question screen (need to keep track at least of the "total Points"

--- app/src/main/java/com/heumehle/zeth/sciencequiz/MainQuizScreen.kt
+++ app/src/main/java/com/heumehle/zeth/sciencequiz/MainQuizScreen.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.content.Intent
 import kotlinx.android.synthetic.main.activity_main_quiz_screen.*
 
 
@@ -21,7 +22,15 @@ class MainQuizScreen : AppCompatActivity() {
     internal var C_Button: Button
     internal var D_Button: Button
 
-    internal var dummyQuestion = Question("Korrekt", "Frage", "Falsch1", "Falsch2", "Falsch3")
+    internal var dummyQuestion1 = Question("Korrekt", "Frage", "Falsch1", "Falsch2", "Falsch3")
+    internal var dummyQuestion2 = Question("2", "Was is 1 + 1?", "3", "4", "5")
+    internal var dummyQuestion3 = Question("Ganz toll", "Wie cool ist Bitconnect?", "1", "500%", "-1000%")
+    internal var dummyQuestion4 = Question("Carlos Matos", "Wer ist der sexiest Man alive?", "Simon", "Lukas", "Philipp")
+    internal var dummyQuestion5 = Question("Angela Merkel", "Ist internet für uns alle Neuland??", "Ja", "Jaha", "Wo ist mein Flugtaxi?")
+
+
+    internal var dummyQuestionHolder = arrayOf(dummyQuestion1, dummyQuestion2, dummyQuestion3, dummyQuestion4, dummyQuestion5)
+    internal var dummyQuestion: Question
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -52,16 +61,17 @@ class MainQuizScreen : AppCompatActivity() {
 
     fun createQuestionScreen() {
 
-        TextField = findViewById<TextView>(R.id.textView2)
-        TextField.setText(dummyQuestion.getM_question())
-        PointsField = findViewById<TextView>(R.id.allPoints)
-        PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
+        dummyQuestion = getDummyQuestion()
+        TextField = findViewById(R.id.textView2)
+        TextField.text = dummyQuestion.m_question
+        PointsField = findViewById(R.id.allPoints)
+        PointsField.text = String.format("Total Points: %s", totalPoints.toString())
 
 
-        A_Button = findViewById<Button>(R.id.answer_A)
-        B_Button = findViewById<Button>(R.id.answer_B)
-        C_Button = findViewById<Button>(R.id.Answer_C)
-        D_Button = findViewById<Button>(R.id.Answer_D)
+        A_Button = findViewById(R.id.answer_A)
+        B_Button = findViewById(R.id.answer_B)
+        C_Button = findViewById(R.id.Answer_C)
+        D_Button = findViewById(R.id.Answer_D)
 
 
         val rand = (Math.random() * 4 + 1).toInt()
@@ -69,68 +79,92 @@ class MainQuizScreen : AppCompatActivity() {
         when (rand) {
 
             1 -> {
-                A_Button.setText(dummyQuestion.getM_correctAnswer())
-                B_Button.setText(dummyQuestion.getM_false1())
-                C_Button.setText(dummyQuestion.getM_false2())
-                D_Button.setText(dummyQuestion.getM_false3())
+                A_Button.text = dummyQuestion.m_correctAnswer
+                B_Button.text = dummyQuestion.m_false1
+                C_Button.text = dummyQuestion.m_false2
+                D_Button.text = dummyQuestion.m_false3
             }
 
             2 -> {
-                B_Button.setText(dummyQuestion.getM_correctAnswer())
-                A_Button.setText(dummyQuestion.getM_false3())
-                C_Button.setText(dummyQuestion.getM_false2())
-                D_Button.setText(dummyQuestion.getM_false1())
+                B_Button.text = dummyQuestion.m_correctAnswer
+                A_Button.text = dummyQuestion.m_false3
+                C_Button.text = dummyQuestion.m_false2
+                D_Button.text = dummyQuestion.m_false1
             }
 
             3 -> {
-                C_Button.setText(dummyQuestion.getM_correctAnswer())
-                A_Button.setText(dummyQuestion.getM_false1())
-                B_Button.setText(dummyQuestion.getM_false2())
-                D_Button.setText(dummyQuestion.getM_false3())
+                C_Button.text = dummyQuestion.m_correctAnswer
+                A_Button.text = dummyQuestion.m_false1
+                B_Button.text = dummyQuestion.m_false2
+                D_Button.text = dummyQuestion.m_false3
             }
 
             4 -> {
-                D_Button.setText(dummyQuestion.getM_correctAnswer())
-                A_Button.setText(dummyQuestion.getM_false2())
-                B_Button.setText(dummyQuestion.getM_false3())
-                C_Button.setText(dummyQuestion.getM_false1())
+                D_Button.text = dummyQuestion.m_correctAnswer
+                A_Button.text = dummyQuestion.m_false2
+                B_Button.text = dummyQuestion.m_false3
+                C_Button.text = dummyQuestion.m_false1
             }
         }
 
-        A_Button.setOnClickListener(View.OnClickListener {
-            if (A_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+        A_Button.setOnClickListener { v ->
+            if (A_Button.text == dummyQuestion.m_correctAnswer) {
                 A_Button.setBackgroundColor(Color.GREEN)
                 totalPoints++
-                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
-            } else
+                PointsField.text = String.format("Total Points: %s", totalPoints.toString())
+                val myIntent = Intent(v.context, CorrectQuestionScreen::class.java)
+                startActivity(myIntent)
+            } else {
                 A_Button.setBackgroundColor(Color.RED)
-        })
+                val myIntent = Intent(v.context, MainQuizScreen::class.java)
+                startActivity(myIntent)
+            }
+        }
 
-        B_Button.setOnClickListener(View.OnClickListener {
-            if (B_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+        B_Button.setOnClickListener { v ->
+            if (B_Button.text == dummyQuestion.m_correctAnswer) {
                 B_Button.setBackgroundColor(Color.GREEN)
                 totalPoints++
-                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
-            } else
+                PointsField.text = String.format("Total Points: %s", totalPoints.toString())
+                val myIntent = Intent(v.context, CorrectQuestionScreen::class.java)
+                startActivity(myIntent)
+            } else {
                 B_Button.setBackgroundColor(Color.RED)
-        })
+                val myIntent = Intent(v.context, MainQuizScreen::class.java)
+                startActivity(myIntent)
+            }
+        }
 
-        C_Button.setOnClickListener(View.OnClickListener {
-            if (C_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+        C_Button.setOnClickListener { v ->
+            if (C_Button.text == dummyQuestion.m_correctAnswer) {
                 C_Button.setBackgroundColor(Color.GREEN)
                 totalPoints++
-                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
-            } else
+                PointsField.text = String.format("Total Points: %s", totalPoints.toString())
+                val myIntent = Intent(v.context, CorrectQuestionScreen::class.java)
+                startActivity(myIntent)
+            } else {
                 C_Button.setBackgroundColor(Color.RED)
-        })
+                val myIntent = Intent(v.context, MainQuizScreen::class.java)
+                startActivity(myIntent)
+            }
+        }
 
-        D_Button.setOnClickListener(View.OnClickListener {
-            if (D_Button.getText() == dummyQuestion.getM_correctAnswer()) {
+        D_Button.setOnClickListener { v ->
+            if (D_Button.text == dummyQuestion.m_correctAnswer) {
                 D_Button.setBackgroundColor(Color.GREEN)
                 totalPoints++
-                PointsField.setText(String.format("Total Points: %s", totalPoints.toString()))
-            } else
+                PointsField.text = String.format("Total Points: %s", totalPoints.toString())
+                val myIntent = Intent(v.context, CorrectQuestionScreen::class.java)
+                startActivity(myIntent)
+            } else {
                 D_Button.setBackgroundColor(Color.RED)
-        })
+                val myIntent = Intent(v.context, MainQuizScreen::class.java)
+                startActivity(myIntent)
+            }
+        }
+    }
+
+    fun getDummyQuestion(): Question {
+        return dummyQuestionHolder[(Math.random() * 5 + 1).toInt()]
     }
 }
